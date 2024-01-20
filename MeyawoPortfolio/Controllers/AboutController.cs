e2e9bb79ecb002baa8c6ba54b4eaa056b79e0bbf
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-01-20 13:29:24 +0300

    Add project files.

---
+++ MeyawoPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateProject() 
+        {
+            return View();
+        }
+    }
+}
